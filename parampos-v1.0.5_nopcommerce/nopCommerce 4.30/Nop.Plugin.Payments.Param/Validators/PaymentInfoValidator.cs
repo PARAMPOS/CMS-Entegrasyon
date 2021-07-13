d4b64ab780388b815bab864b0cc8dac6eb2d1d32
gencnesil <gencnesil@live.com>
2021-07-13 18:54:56 +0300

    Nopecommerce 4.30 plugin added

---
+++ parampos-v1.0.5_nopcommerce/nopCommerce 4.30/Nop.Plugin.Payments.Param/Validators/PaymentInfoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Nop.Plugin.Payments.Param.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Plugin.Payments.Param.Validators
+{
+    public class PaymentInfoValidator : AbstractValidator<PaymentInfoModel>
+    {
+        public PaymentInfoValidator(ILocalizationService localizationService)
+        {
+            RuleFor(x => x.CardholderName).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardholderName.Required"));
+            RuleFor(x => x.CardNumber).IsCreditCard().WithMessage(localizationService.GetResource("Payment.CardNumber.Wrong"));
+            RuleFor(x => x.CardCode).Matches(@"^[0-9]{3,4}$").WithMessage(localizationService.GetResource("Payment.CardCode.Wrong"));
+        }
+    }
+}
