19dfec3935da338ad3ea275b2d400c0ccd9ccf0e
gencnesil <gencnesil@live.com>
2021-08-15 15:41:04 +0300

    Currency 3D update

--- parampos-v1.0.4_woocommerce/turkpos/Model/Api/TP_Islem_Odeme_WD.php
+++ parampos-v1.0.4_woocommerce/turkpos/Model/Api/TP_Islem_Odeme_WD.php
@@ -8,7 +8,7 @@ class TP_Islem_Odeme_WD extends Pos_Odeme
     {
         parent:: __construct($CLIENT_CODE,$CLIENT_USERNAME,$CLIENT_PASSWORD,$sPosId,$guid,$kkSahibi,$kkNo,$kkSkAy,$kkSkYil,$kkCvc,$kkSahibiGsm,$hataUrl,$basariliUrl,$siparisId,$siparisAciklama,$taksit,$islemtutar,$toplamTutar,$islemid,$ipAdr,$RefUrl,$dataBir,$dataIki,$dataUc,$dataDort,$dataBes);
         
-        $this->Islem_Guvenlik_Tip = 'NS';
+        $this->Islem_Guvenlik_Tip = '3D';
 
         $currency = get_option('woocommerce_currency');
         switch ($currency) {
